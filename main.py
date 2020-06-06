879f0ed916fb85146e416a228b3da3e6541af063
Batelier <baptiste.roberjot@isep.fr>
2020-06-06 20:17:28 +0200

    change loaded img

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from src.mask import Mask
 print("Interactive Graph Cut for Computer Vision \nBased on Jolly and and Boykov Paper \n"
       "--------------------------------------------------------------------------------")
 
-filename = 'src/img_test/mouette.png'
+filename = 'src/img_test/lotus.jpg'
 
 mask = Mask().makeMask(filename)
 print(mask)
