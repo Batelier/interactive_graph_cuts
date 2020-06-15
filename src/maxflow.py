e19cce584b15aeda9bc109519dcbb10244e3bf2a
Batelier <baptiste.roberjot@isep.fr>
2020-06-15 18:11:34 +0200

    smallest pathflow over a path

--- src/maxflow.py
+++ src/maxflow.py
@@ -10,17 +10,22 @@ def Ford_Fulkerson(graph, source, sink):
     residualGraph = graph #graph we will modify to determine the maxflow
     maxFlow = 0
 
-    while(bfs(residualGraph, source, sink, parentNode)): #if we find a path in the residual graph, execute ford-fulkerson
+    #while we find a path in the residual graph, execute ford-fulkerson
+    while(bfs(residualGraph, source, sink, parentNode)):
 
         pathFlow = INF
         v = sink
 
-        while v != source:
+        #find the smallest pathFlow of the current path
+        while v != source: #go to the previous node until we get back to the source
             u = parentNode[v]
-            pathFlow = min(pathFlow, residualGraph[u][v])
+            pathFlow = min(pathFlow, residualGraph[u][v]) #get the smallest value of the past in order to make the soustraction
             v = parentNode[v]
 
+        #substract the weight of the smallest pathFlow to the other weight of the path
         v = sink
+        while v != source:
+            u = parentNode
 
 
 
